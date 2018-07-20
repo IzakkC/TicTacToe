022b7c8dab87eb98fac22ad264e71eb3834056df
Izakk Carrillo <izakkray@gmail.com>
2018-07-20 13:30:31 -0400

    Finished the game

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -43,35 +43,99 @@ class ViewController: UIViewController {
         button1.isEnabled = false
     }
     @IBAction func button2(_ sender: Any) {
-        print("Button 2 tapped")
+        if isXTurn {
+            button2.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button2.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        
+        isXTurn = !isXTurn
+        
+        button2.isEnabled = false
     }
     
     @IBAction func button3tapped(_ sender: Any) {
-        print("Button 3 tapped")
+        if isXTurn {
+            button3.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button3.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        
+        isXTurn = !isXTurn
+        
+        button3.isEnabled = false
     }
     
     @IBAction func button4tapped(_ sender: Any) {
-        print("Button 4 tapped")
+        if isXTurn {
+            button4.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button4.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        
+        isXTurn = !isXTurn
+        
+        button4.isEnabled = false
     }
     
     @IBAction func button5tapped(_ sender: Any) {
-        print("Button 5 tapped")
+        if isXTurn {
+            button5.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button5.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        
+        isXTurn = !isXTurn
+        
+        button5.isEnabled = false
     }
     
     @IBAction func button6tapped(_ sender: Any) {
-        print("Button 6 tapped")
+        if isXTurn {
+            button6.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button6.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        
+        isXTurn = !isXTurn
+        
+        button6.isEnabled = false
     }
     
     @IBAction func button7tapped(_ sender: Any) {
-        print("Button 7 tapped")
+        if isXTurn {
+            button7.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button7.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        
+        isXTurn = !isXTurn
+        
+        button7.isEnabled = false
     }
     
     @IBAction func button8tapped(_ sender: Any) {
-        print("Button 8 tapped")
+        if isXTurn {
+            button8.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button8.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        
+        isXTurn = !isXTurn
+        
+        button8.isEnabled = false
     }
     
     @IBAction func button9tapped(_ sender: Any) {
-        print("Button 9 tapped")
+        if isXTurn {
+            button9.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button9.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        
+        isXTurn = !isXTurn
+        
+        button9.isEnabled = false
     }
     
     
